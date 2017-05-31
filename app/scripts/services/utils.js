c6246cc60c620685a52ad7cd89fb77da8a20f682
nandilagos <lagos.jara.a@gmail.com>
2017-05-31 16:28:24 -0400

    fix

--- app/scripts/services/utils.js
+++ app/scripts/services/utils.js
@@ -67,7 +67,6 @@ angular.module('efindingAdminApp')
 
 			$auth.setToken(success.data.attributes.access_token);
 			that('refresh_t', success.data.attributes.refresh_token);
-			$log.error('bien');
 
 			functionToCall({
 				success: true,
@@ -80,7 +79,6 @@ angular.module('efindingAdminApp')
 				success: false,
 				detail: error
 			});
-			$log.error('mal');
 			$log.error(error);
 			// $state.go('core.login');
 		});
