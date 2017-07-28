04b51ce168ab03831f5503c282994576c7ed0645
nandilagos <lagos.jara.a@gmail.com>
2017-07-28 15:40:34 -0400

    production

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -95,8 +95,8 @@ angular
 .config(['$authProvider',
 	function($authProvider) {
 		// Parametros de configuración
-		//$authProvider.loginUrl = 'http://50.16.161.152/efinding/oauth/token'; 	//Produccion
-		$authProvider.loginUrl = 'http://50.16.161.152/pitagora/oauth/token'; 	//Pitagora
+		$authProvider.loginUrl = 'http://50.16.161.152/efinding/oauth/token'; 	//Produccion
+		//$authProvider.loginUrl = 'http://50.16.161.152/pitagora/oauth/token'; 	//Pitagora
 		//$authProvider.loginUrl = 'http://localhost:3000/oauth/token'; 					//Local
 		$authProvider.tokenName = 'access_token';
 	}
